41b966b512b822781676c256b211228399adb075
phconzatti <phconzatti@hotmail.com>
2025-09-03 20:37:32 -0300

    .

---
+++ src/main/java/com/example/demo/InformationCollector.java
@@ -0,0 +1,105 @@
+package com.example.demo;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class InformationCollector {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+    private LocalDateTime dataHora;
+    
+    public InformationCollector() {
+
+    }
+
+
+    public InformationCollector(long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, BigDecimal totalCalculatedAmount, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.quantity = quantity;
+        this.conversionMultiple = conversionMultiple;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.environment = environment;
+        this.dataHora = LocalDateTime.now();
+    }
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return this.conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getTotalCalculatedAmount() {
+        return this.totalCalculatedAmount;
+    }
+
+    public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+        this.totalCalculatedAmount = totalCalculatedAmount;
+    }
+
+    public String getEnvironment() {
+        return this.environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public LocalDateTime getDataHora() {
+        return this.dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+    
+}
